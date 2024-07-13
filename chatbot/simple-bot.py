bc48d87205b1c7258068f2262f8e17236ca7147f
ernajonarratt <163578328+ernajonarratt@users.noreply.github.com>
2024-07-13 13:01:10 +0200

    Update simple-bot.py

--- chatbot/simple-bot.py
+++ chatbot/simple-bot.py
@@ -4,10 +4,10 @@ from wechaty import Wechaty, Message
 async def on_message(msg: Message):
     if msg.text() == 'ding':
         await msg.say('dong')
-    
+    .
 async def main():
     bot = Wechaty()
     bot.on('message', on_message)
     await bot.start()
 
-asyncio.run(main())
+asyncio.run(main()).
